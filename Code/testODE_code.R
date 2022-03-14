952407dae78fa15d8a9b2dbc7585d576c6678800
MarcoFelipeKing <51195817+MarcoFelipeKing@users.noreply.github.com>
2022-03-14 16:12:45 +0000

    adaptive delta

--- Code/testODE_code.R
+++ Code/testODE_code.R
@@ -1,35 +1,35 @@
-library(deSolve)
-library(scatterplot3d)
-
-testODE <- function(time_space, initial_contamination, parameters){
-  with(
-    as.list(c(initial_contamination, parameters)),{
-      dContamination <- -d*exp(-g*time_space)*Contamination
-      return(list(dContamination))
-    }
-  )
-}
-
-parameters <- c(C = -8/3, d = -10, g =  28)
-Y=c(y=1200)
-times <- seq(0, 6, by = 0.01)
-initial_contamination=c(Contamination=1200) 
-out <- ode(initial_contamination, times, testODE, parameters, method = "radau",atol = 1e-4, rtol = 1e-4)
-
-plot(out)
-
-
-library(deSolve)
-library(scatterplot3d)
-dyn.load("Code/testODE.so")
-
-Y <-c(y1=initial_contamination) ;
-out <- ode(1200, times, func = "derivs", parms = parameters,
-           dllname = "testODE", initfunc = "initmod")
-
-
-plot(out)
-
+# library(deSolve)
+# library(scatterplot3d)
+# 
+# testODE <- function(time_space, initial_contamination, parameters){
+#   with(
+#     as.list(c(initial_contamination, parameters)),{
+#       dContamination <- -d*exp(-g*time_space)*Contamination
+#       return(list(dContamination))
+#     }
+#   )
+# }
+# 
+# parameters <- c(C = -8/3, d = -10, g =  28)
+# Y=c(y=1200)
+# times <- seq(0, 6, by = 0.01)
+# initial_contamination=c(Contamination=1200) 
+# out <- ode(initial_contamination, times, testODE, parameters, method = "radau",atol = 1e-4, rtol = 1e-4)
+# 
+# plot(out)
+# 
+# 
+# library(deSolve)
+# library(scatterplot3d)
+# dyn.load("Code/testODE.so")
+# 
+# Y <-c(y1=initial_contamination) ;
+# out <- ode(1200, times, func = "derivs", parms = parameters,
+#            dllname = "testODE", initfunc = "initmod")
+# 
+# 
+# plot(out)
+# 
 
 
 library(deSolve)
@@ -43,8 +43,10 @@ testODE <- function(t, y, parameters){
   )
 }
 
-system("R CMD SHLIB Code/testODE.c")
-dyn.load("Code/testODE.so")
+setwd(getwd())
+
+system("R CMD SHLIB testODE_X86.c")
+dyn.load("testODE_X86.so")
 
 parameters <- c(C = 2000, d = 100, g =  50, r=50)
 initial_contamination<- c(Contamination = 1200)
@@ -52,13 +54,13 @@ times      <- seq(0, 6, by = 0.01)
 
 out1 <- ode(initial_contamination, times, testODE,
             parms = parameters, method = "radau", atol = 1e-4, rtol = 1e-4)
-out2 <- ode(initial_contamination, times, func = "derivs", dllname = "testODE", initfunc = "initmod",
+out2 <- ode(initial_contamination, times, func = "derivs", dllname = "testODE_X86", initfunc = "initmod",
             parms = parameters, method = "radau", atol = 1e-4, rtol = 1e-4)
 
 plot(out1, out2)          # no visible difference
 summary(out1 - out2)      # differences should be (close to) zero
 
-dyn.unload("Code/testODE.so") # always unload before editing .c file !!
+dyn.unload("testODE_X86.so") # always unload before editing .c file !!
 
 
 
